535b103ca55a8d0a2b3b67075248fbf2d66f7e56
karolux4 <karolisbutkus4@gmail.com>
2020-10-31 13:51:01 +0200

    Added main page with styles, javascript files and controller

--- IS_Turizmas/Startup.cs
+++ IS_Turizmas/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +17,10 @@ namespace IS_Turizmas
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting=false);
+
+
+            //services.AddDbContext<DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +31,7 @@ namespace IS_Turizmas
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            /*app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
@@ -34,6 +39,14 @@ namespace IS_Turizmas
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+            });*/
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=HOME}/{action=INDEX}/{id?}");
             });
         }
     }
