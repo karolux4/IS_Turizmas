f96ae8ac36f2c1399ed6482240eadb270f84781a
karolux4 <karolisbutkus4@gmail.com>
2020-11-02 19:02:15 +0200

    Added Geocoding API

--- IS_Turizmas/Controllers/RoutesController.cs
+++ IS_Turizmas/Controllers/RoutesController.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace IS_Turizmas.Controllers
 {
@@ -18,11 +25,56 @@ namespace IS_Turizmas.Controllers
             return View();
         }
 
+        public IActionResult AddRouteObjects()
+        {
+            return View();
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> CreateDescription()
         {
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddObjects(string address)
+        {
+            HttpClient client = new HttpClient();
+
+            string line = "address:Kaunas&key=AIzaSyDL5U4rZ7pfwLxxlRWy85rXflMJ93TC5mI";
+            string encoded = HttpUtility.UrlEncode(line, Encoding.UTF8);
+
+            string url = "https://maps.googleapis.com/maps/api/geocode/json?";
+            url += HttpUtility.UrlPathEncode("address="+address);
+            url += HttpUtility.UrlPathEncode("&key=AIzaSyDL5U4rZ7pfwLxxlRWy85rXflMJ93TC5mI");
+
+
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+
+            };
+
+            
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            if(response.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Blogai");
+            }
+            else
+            {
+                var responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                Console.WriteLine(responseBody);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+       
     }
 }
