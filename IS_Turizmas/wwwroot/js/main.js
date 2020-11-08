20634149cc13e531496e867945897b47a8207cdd
karolux4 <karolisbutkus4@gmail.com>
2020-11-08 14:40:44 +0200

    Created route uniqueness form

--- IS_Turizmas/wwwroot/js/main.js
+++ IS_Turizmas/wwwroot/js/main.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function () {
-
+    var date_input = $('.input-daterange');
+    var options = {
+        format: "yyyy-mm-dd",
+        todayHighlight: true,
+        todayBtn: "linked",
+        language: "lt",
+        autoclose: true,
+    };
+    date_input.datepicker(options);
 });
