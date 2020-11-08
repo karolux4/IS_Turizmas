fb4d614ac4ac61ba362a25a85217731b9542ed26
Symuciakas <sixas10@gmail.com>
2020-11-08 20:41:20 +0200

    Symuciako dalis

---
+++ IS_Turizmas/Controllers/AdvertsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IS_Turizmas.Controllers
+{
+    public class AdvertsController : Controller
+    {
+        public IActionResult Confirm()
+        {
+            return View();
+        }
+
+        public IActionResult AdvertList()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAdvert()
+        {
+            return View();
+        }
+        
+        public IActionResult CreateAdvertPlan()
+        {
+            return View();
+        }
+        public IActionResult DeleteAdvertPlan()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAdvertStatistics()
+        {
+            return View();
+        }
+
+        public IActionResult EditAdvert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ConfirmActivation()
+        {
+            return RedirectToAction(nameof(AdvertList));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AdvertCreate()
+        {
+            return RedirectToAction(nameof(AdvertList));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AdvertPlanCreate()
+        {
+            return RedirectToAction(nameof(Confirm));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AdvertEdit()
+        {
+            return RedirectToAction(nameof(AdvertList));
+        }
+    }
+}
