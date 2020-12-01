4b38be9d00913cfda9713f2172898962823561fd
SimonasBan <sbansevicius@gmail.com>
2020-12-01 22:14:29 +0200

    Show all routes

--- IS_Turizmas/Controllers/ContentController.cs
+++ IS_Turizmas/Controllers/ContentController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using IS_Turizmas.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -17,10 +18,30 @@ namespace IS_Turizmas.Controllers
 {
     public class ContentController : Controller
     {
-
-        public IActionResult ViewAllRoutes()
+        private readonly ApplicationDbContext _context;
+        //SqlCommand com = new SqlCommand();
+        //SqlDataReader dr;
+        //SqlConnection con = new SqlConnection();
+        public ContentController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> ViewAllRoutes()
+        {
+            return View(await _context.Marsrutai.Include(o => o.MarsrutoObjektai).ToListAsync());
+        }
+
+        //public IActionResult ViewAllRoutes()
+        //{
+        //    //var movie = new Movie() { Name = "Test" };
+        //    //var test = "Testas";
+        //    //return View();
+        //    return View(_context.Marsrutai.Include(o => o.MarsrutoObjektai));
+        //}
+        private void FetchAllData()
+        {
+
         }
 
 
