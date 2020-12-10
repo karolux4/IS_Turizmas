5318007dc64404d6ee98fa1d18ebada19f30db7e
SimonasBan <sbansevicius@gmail.com>
2020-12-10 15:32:22 +0200

    Added Comment writing

--- IS_Turizmas/Controllers/ContentController.cs
+++ IS_Turizmas/Controllers/ContentController.cs
@@ -21,9 +21,6 @@ namespace IS_Turizmas.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly SignInManager<RegistruotiVartotojai> _signInManager;
-        //SqlCommand com = new SqlCommand();
-        //SqlDataReader dr;
-        //SqlConnection con = new SqlConnection();
         public ContentController(ApplicationDbContext context,
             SignInManager<RegistruotiVartotojai> signInManager)
         {
@@ -36,23 +33,19 @@ namespace IS_Turizmas.Controllers
             return View(await _context.Marsrutai.Include(o => o.MarsrutoObjektai).ToListAsync());
         }
 
-        //public IActionResult ViewAllRoutes()
-        //{
-        //    //var movie = new Movie() { Name = "Test" };
-        //    //var test = "Testas";
-        //    //return View();
-        //    return View(_context.Marsrutai.Include(o => o.MarsrutoObjektai));
-        //}
+
         private void FetchAllData()
         {
 
         }
 
-
-        public IActionResult ViewRouteInfo()
+        public async Task<IActionResult> ViewRouteInfo(int id)
         {
-            return View();
+            int test = id;
+            return View(await _context.Marsrutai.FirstOrDefaultAsync(o => o.Id == id));
         }
+
+
 
         public IActionResult ViewAllRouteAnalysis()
         {
@@ -92,11 +85,6 @@ namespace IS_Turizmas.Controllers
             //return View(await _context.Marsrutai.Where(obj => EF.Functions.Like(obj.Pavadinimas, "%" + searchText + "%")).ToListAsync);
         }
 
-        //public IActionResult FoundRoutes()
-        //{
-        //    return View();
-        //}
-
         public IActionResult ShareRoute()
         {
             return View();
@@ -108,11 +96,50 @@ namespace IS_Turizmas.Controllers
         }
 
 
-        public IActionResult Comment()
+        public async Task<IActionResult> Comment(int Id)
         {
+            ViewBag.id = Id;
+
+            bool auth = _signInManager.Context.User.Identity.IsAuthenticated;
+
+            if (!auth)
+            {
+                TempData["ErrorMessage"] = "Kad rašytumėte komentarą, turite prisijungti.";
+                return RedirectToAction("ViewRouteInfo", new { id = Id });
+            }
+
             return View();
         }
 
-        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateComment([Bind("Tekstas, FkMarsrutas")] Komentarai comment)
+        {
+            int userId = Int32.Parse(_signInManager.UserManager.GetUserId(this.User));
+
+            comment.Data = DateTime.Now;
+            comment.FkRegistruotasVartotojas = userId;
+            int routeId = comment.FkMarsrutas;           
+
+
+            try
+            {
+                _context.Komentarai.Add(comment);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                return NotFound();
+                throw;
+            }
+
+            
+
+            TempData["SuccessMessage"] = "Komentaras sukurtas";
+            return RedirectToAction("ViewRouteInfo", new { id = routeId } );
+        }
+
+
+
     }
 }
